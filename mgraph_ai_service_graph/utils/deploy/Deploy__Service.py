3a7495064f662b5f2ef961411351fd3d27764bb2
Dinis Cruz <dinis.cruz@owasp.org>
2025-11-28 01:25:27 +0000

    updated lambda dependencies and wired up the cache service env var :)

--- mgraph_ai_service_graph/utils/deploy/Deploy__Service.py
+++ mgraph_ai_service_graph/utils/deploy/Deploy__Service.py
@@ -1,5 +1,6 @@
 from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API   import Deploy__Serverless__Fast_API
-from mgraph_ai_service_graph.config import SERVICE_NAME, LAMBDA_DEPENDENCIES__GRAPH_SERVICE
+from osbot_utils.utils.Env                                           import get_env
+from mgraph_ai_service_graph.config                                  import SERVICE_NAME, LAMBDA_DEPENDENCIES__GRAPH_SERVICE
 from mgraph_ai_service_graph.fast_api.lambda_handler                 import run
 
 class Deploy__Service(Deploy__Serverless__Fast_API):
@@ -8,6 +9,11 @@ class Deploy__Service(Deploy__Serverless__Fast_API):
         with super().deploy_lambda() as _:
             # Add any service-specific environment variables here
             # Example: _.set_env_variable('BASE_API_KEY', get_env('BASE_API_KEY'))
+            #FAST_API__AUTH__API_KEY__NAME
+            #FAST_API__AUTH__API_KEY__VALUE
+            _.set_env_variable("AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME" , get_env("AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_NAME"  ))
+            _.set_env_variable("AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE", get_env("AUTH__TARGET_SERVER__CACHE_SERVICE__KEY_VALUE" ))
+            _.set_env_variable("URL__TARGET_SERVER__CACHE_SERVICE"            , get_env("URL__TARGET_SERVER__CACHE_SERVICE"             ))
             return _
 
     def handler(self):
